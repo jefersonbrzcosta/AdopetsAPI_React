1270286349bef84a60fe987cd8d78ab0bc230b08
Jeferson Costa <jefersonbrz.costa@gmail.com>
2019-12-27 14:13:00 -0300

    Card added

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,12 +1,35 @@
 import React from "react";
+import Male from "../../img/dog_boy_Avatar.PNG";
+import Female from "../../img/dog_girl_Avatar.PNG";
+import { Pagination } from "antd";
 
-import { Container, Filter, Results } from "./styles";
+import { Container, Filter, Results, Card, Wrapper } from "./styles";
 
 export default function Main() {
   return (
     <Container>
+      <img
+        src="https://www.adopets.com/static/media/adopets-primary.40eeabc5.svg"
+        alt
+      />
       <Filter>Options</Filter>
-      <Results></Results>
+      <Results>
+        <Wrapper>
+          <Card>
+            <div>
+              <img src={Male} alt="avatar" />
+              <strong>Bug</strong>
+              <p>Border Terrier</p>
+            </div>
+          </Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+        </Wrapper>
+        <Pagination className="colorUP" defaultCurrent={1} total={35} />
+      </Results>
     </Container>
   );
 }
--- src/Components/Main/styles.js
+++ src/Components/Main/styles.js
@@ -6,9 +6,15 @@ export const Container = styled.div`
   background-color: #ebebeb;
   display: flex;
   align-content: center;
-  align-items: center
+  align-items: center;
   justify-content: center;
   height: 100vh;
+
+  > img {
+    position: absolute;
+    top: 0;
+    margin-top: 20px;
+  }
 `;
 
 export const Filter = styled.div`
@@ -17,7 +23,7 @@ export const Filter = styled.div`
   font-size: 15px;
   font-weight: bold;
   align-items: center;
-  flex-direction: column
+  flex-direction: column;
   width: 250px;
   padding: 10px;
   border-radius: 10px;
@@ -27,13 +33,40 @@ export const Filter = styled.div`
 export const Results = styled.div`
   background-color: white;
   display: flex;
-  font-size:15px;
+  font-size: 15px;
   font-weight: bold;
+  flex-direction: column;
   align-items: center;
-  flex-direction: column
   width: 600px;
   padding: 10px;
   border-radius: 10px;
   height: 70%;
   margin-left: 15px;
+`;
+
+export const Card = styled.div`
+  background-color: #ffdeea;
+  display: flex;
+  flex-direction: column;
+  width: 170px;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
+  height: 150px;
+
+  img {
+    width: 35px;
+    border-radius: 30px;
+    float: right;
+  }
+  p {
+    font-size: 10px;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-bottom: 20px;
 `;
